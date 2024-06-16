7647f2cbaae35d2007fbb8769f1d08362733931a
efeakyurt <88876644+efeakyurt@users.noreply.github.com>
2024-06-16 09:24:38 +0300

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -7,7 +7,6 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Do not change code above this line. Use the PSQL variable above to query your database.
 
 # Truncate the tables
 echo "$($PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY")"
